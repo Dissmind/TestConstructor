3376fa047de6aa2fe8608d8785812d9eea3da527
Dissmind <dissmind@gmail.com>
2020-10-27 19:43:14 +0200

    [ADD]: models Question, Test

---
+++ src/Model/Question.js
@@ -0,0 +1,7 @@
+export class Question {
+    constructor(title, answers, correctAnswer) {
+        this.title = title
+        this.answers = answers
+        this.correctAnswer = correctAnswer
+    }
+}
---
+++ src/Model/Test.js
@@ -0,0 +1,7 @@
+export class Test {
+    constructor(id, title, question) {
+        this.id = id
+        this.title = title
+        this.questions = question
+    }
+}
