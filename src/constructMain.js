aec9f3cab37a0755bb222720968a1c26f67b1763
SIDExSIDE <anoshkinzz@mail.ru>
2020-10-30 17:46:28 +0300

    Add constructor

--- src/constructMain.js
+++ src/constructMain.js
@@ -1,1 +1,58 @@
-console.log('asdas')
+import { Constructor } from './constuctor/Constructor.js'
+
+const $btnAddQuestion = document.querySelector('.btnAddQuestion')
+const $btnAddAnswer = document.querySelector('.btnAddAnswer')
+const $btnConfirm = document.querySelector('.btnConfirm')
+const $answers = document.querySelector('.answers')
+
+
+let questions = [] 
+
+const constructor = new Constructor()
+
+function btnAddQuestionHandler() {
+    const questionTitle = '.questionTitle'
+    const answerTitle = '.answerTitle'
+    const correctAnswer = '.correctAnswer'
+
+    const question = constructor.addQuestion(questionTitle, answerTitle, correctAnswer)
+    questions.push(question)
+
+    constructor.clear(questionTitle, answerTitle, correctAnswer)
+}
+
+function btnAddAnswerHandler() {
+    const idButton = 'answer-button-' + Math.round(Math.random() * (10000 - 1) + 1);
+    const idAnswer = 'answer-' + Math.round(Math.random() * (10000 - 1) + 1);
+
+    $answers.insertAdjacentHTML('beforeend', `
+        <div class="added answer" id="${idAnswer}">
+            <input type="text" class="answerTitle" placeholder="Ответ">
+            <input type="radio" class="correctAnswer" name="correctAnswer">
+            <button id="${idButton}" class="btnDelAnswer">-</button>
+        </div>
+    `)
+
+    document.querySelector('#' + idButton)
+        .addEventListener('click', () => { btnDelAnswerHandler(idAnswer) })
+}
+
+function btnDelAnswerHandler(id) {
+    const $addedAnswer = document.querySelector('#' + id)
+    $addedAnswer.parentNode.removeChild($addedAnswer)
+}
+
+function btnConfirmHandler() {
+    btnAddQuestionHandler()
+
+    // TODO: Отправка данных на сервер (questions[])
+
+    console.log(questions)
+    questions = []
+}
+
+$btnAddQuestion.addEventListener('click', btnAddQuestionHandler)
+$btnAddAnswer.addEventListener('click', btnAddAnswerHandler)
+$btnConfirm.addEventListener('click', btnConfirmHandler)
+
+//TODO: метод - строка пуста (или пробелы) - true, иначе false
